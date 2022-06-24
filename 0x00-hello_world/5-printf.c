1544d833286f9b0394c6953cc1b6ea406e47d954
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-06-24 08:14:57 -0700

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/* Usage of printf */
+
+#include <stdio.h>
+
+/**
+ * main - prints plain text
+ *
+ * Return: 0 in success
+ *
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	/* Return: 0 in success */
+	return (0);
+}
