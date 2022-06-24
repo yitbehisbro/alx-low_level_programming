dd8b3fd0525b4189f26d8c43d3a5aba7a89024ee
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-06-24 07:51:57 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /* main : prints a text on the console */
-void main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	/* Return - returns 0 in success */
