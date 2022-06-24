7ec7ee26c0e7367181de6d1b977ef2201ab2f654
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-06-24 08:38:38 -0700

    Printf type size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+/* Size of data types */
+
+#include <stdio.h>
+
+/**
+ * main - prints size of diffrent datatypes
+ *
+ * Return: 0 in success
+ *
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longInt;
+	long long int long2Int;
+	float floatType;
+	
+	/* Prints size of data types */
+	printf("Size of a char: %zu bytes\n", sizeof(charType));
+	printf("Size of an int: %zu bytes\n", sizeof(intType));
+	printf("Size of a long int: %zu bytes\n", sizeof(longInt));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long2Int));
+	printf("Size of a float: %zu bytes\n", sizeof(floatType));
+
+	/* Return: 0 in success */
+	return (0);
+}
