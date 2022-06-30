1b00a37397423fcd6623ea6198cfcbce1d931c19
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-06-30 03:28:06 -0700

    let me try

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/* Prints the text */
+
+#include <stdio.h>
+
+/**
+ * main - prints the text to the console
+ *
+ * Return: 0 in success
+ */
+int main(void)
+{
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	/* Return: 1 for error message */
+	return (1);
+}
