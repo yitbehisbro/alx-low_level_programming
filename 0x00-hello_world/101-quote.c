51fbc23c8b0c690293c4ad10f226134bbdf46c56
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-06-30 03:44:43 -0700

    Print texts without using printf and puts function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
-/* Prints the text */
-{
-#include <stdio.h>
+/* Prints text */
+
+#include <unistd.h>
 
 /**
- * main - prints the text to the console
+ * main - prints text to the console
  *
  * Return: 1 in success
+ *
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	/* Return: 1 for error message */
+	write(2, text, 59);
 	return (1);
-}
 }
