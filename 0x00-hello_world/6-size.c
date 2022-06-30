d13553c1f4f56630553313977dc635047f763f0a
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-06-30 11:52:14 +0300

    Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,11 +19,11 @@ int main(void)
 	 */
 
 	/* Prints size of data types */
-	printf("Size of a char: %zu bytes\n", sizeof(char));
-	printf("Size of an int: %zu bytes\n", sizeof(int));
-	printf("Size of a long int: %zu bytes\n", sizeof(long int));
-	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
-	printf("Size of a float: %zu bytes\n", sizeof(float));
+	printf("Size of a char: %zu bytes\n", sizeof(char*));
+	printf("Size of an int: %zu bytes\n", sizeof(int*));
+	printf("Size of a long int: %zu bytes\n", sizeof(long int*));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long long int*));
+	printf("Size of a float: %zu bytes\n", sizeof(float*));
 
 	/* Return: 0 in success */
 	return (0);
