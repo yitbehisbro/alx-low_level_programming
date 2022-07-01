31b71005b0c12c43f6208c108b37a77c8c3fda5b
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-07-01 18:59:05 +0300

    updates

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,15 @@ int main(void)
 	{
 		for (b = a + 1; b <= 99; b++)
 		{
-			putchar(a + '0');
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
 			putchar(' ');
-			putchar(b + '0');
-			if ((a == 98) && (b == 99))
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 98 && a == 99)
 				continue;
+
 			putchar(',');
 			putchar(' ');
 		}
