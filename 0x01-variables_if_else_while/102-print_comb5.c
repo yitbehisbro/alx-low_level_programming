afba62babc45b3ee1c357d3b0286744e212188e4
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-07-01 18:36:36 +0300

    updates

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
-/* Filename: 100-print_comb3.c AND Author: Yitbarek Wendimu */
+/* Filename: 102-print_comb5.c AND Author: Yitbarek Wendimu */
 
 #include <stdio.h>
 
 /**
- * main - Prints the three digits combination meaningfully
+ * main - Prints the four digits combination meaningfully
  *
  * Return: 0 in success
  *
@@ -13,20 +13,24 @@ int main(void)
 	int a;
 	int b;
 	int c;
+	int d;
 
-	for (a = 0; a <= 8; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = a + 1; b < 10; b++)
+		for (b = a; b < 9; b++)
 		{
-			for (c = b + 1; c < 10; c++)
+			for (c = a; c < 10; c++)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(c + '0');
-				if ((b == 8) && (a == 7) && (c == 9))
-				continue;
-				putchar(',');
-				putchar(' ');
+				for (d = a + 1; d < 10; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					if ((b == 8) && (a == 9) && (c == 9) && (d == 9))
+						continue;
+					putchar(',');
+					putchar(' ');
 			}
 		}
 	}
