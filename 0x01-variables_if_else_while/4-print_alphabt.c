d0ef942b829cc5c1d13029ac72fab285348eb832
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-07-01 15:46:41 +0300

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - prints character a to z
+ *
+ * Return: 0 in success
+ *
+ */
+int main(void)
+{
+    char k;
+
+    for (k = 'A'; k <= 'Z'; k++)
+    {  
+      putchar(tolower(k));
+    }
+    putchar('\n');
+    return (0);
+}
