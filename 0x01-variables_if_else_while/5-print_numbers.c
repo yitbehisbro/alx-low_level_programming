8720a363aea92599d0c84b4fa2f9ed5de271aaab
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-01 06:21:37 -0700

    Print 0 through 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 through 8
+ *
+ * Return: 0 in success
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
