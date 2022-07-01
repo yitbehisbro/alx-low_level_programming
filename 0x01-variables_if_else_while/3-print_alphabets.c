384a7ebb4a2788960a3929d52689434773de358b
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-01 05:22:39 -0700

    up

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <ctype.h>
+
+
+
+/**
+
+ * main - prints character a to z
+
+ *
+
+ * Return: 0 in success
+
+ *
+
+ */
+
+int main(void)
+
+{
+
+	char k;
+
+
+
+	for(k = 'A'; k <= 'Z'; k++)
+
+	{
+
+		putchar(tolower(k));
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
