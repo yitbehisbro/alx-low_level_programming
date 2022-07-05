6e5468123c1cec859db7c9dba3371565c464990a
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-07-05 19:43:58 +0300

    Updates

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,35 +10,35 @@
  */
 void print_times_table(int n)
 {
-	int a, b, op;
+	int k, j, s;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; a++)
+		for (k = 0; k <= n; k++)
 		{
 			_putchar(48);
-			for (b = 1; b <= n; b++)
+			for (j = 1; j <= n; j++)
 			{
-				op = a * b;
+				s = k * j;
 				_putchar(44);
 				_putchar(32);
-				if (op <= 9)
+				if (s <= 9)
 				{
 					_putchar(32);
 					_putchar(32);
-					_putchar(op + 48);
+					_putchar(s + 48);
 				}
-				else if (op <= 99)
+				else if (s <= 99)
 				{
 					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar((s / 10) + 48);
+					_putchar((s % 10) + 48);
 				}
 				else
 				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(((s / 100) % 10) + 48);
+					_putchar(((s / 10) % 10) + 48);
+					_putchar((s % 10) + 48);
 				}
 			}
 			_putchar('\n');
