3089eb61fd0628a4ba827804fed872354bd19c2f
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-05 05:52:26 -0700

    Small letters in alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints small letters
+ *
+ * Return: 0 in success
+ *
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
