ed4b75f5f78acbf12dd067d513bbb5d8327fa45d
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-05 07:09:38 -0700

    Print the sign of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of the numbers
+ *
+ * @n: variable for numbers or int values
+ *
+ * Return: 1 if n > 0; 0 if n = 0; -1 if n < 0
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(+);
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('\0');
+	}
+}
