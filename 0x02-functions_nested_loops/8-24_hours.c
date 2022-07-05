e9f7e46c77c187668453397b77fb77a2a2503964
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-05 08:35:22 -0700

    Prints min in 24 hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints minute in 24 hr
+ *
+ * Return: 0 in success
+ *
+ */
+void jack_bauer(void)
+{
+	int w, x;
+
+	for (w = 0; w < 24; w++)
+	{
+		for (x = w; x <= 59; x++)
+		{
+			_putchar((w / 10) + '0');
+			_putchar((w % 10) + '0');
+			_putchar(':');
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+
+			if ((w == 24) && (x == 59))
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
