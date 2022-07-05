056ff52964ce7f070281cfe340c1e1dd2e680261
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-07-05 16:35:15 +0300

    Updates

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,9 @@
 /**
  * _islower - Checks whether the letter is lowercase or not
  *
- * Return: 0 in success
+ * Return: 0 in if false 1 if true
+ *
+ * @c: the variable which represents ASCII code or num values
  *
  */
 int _islower(int c)
