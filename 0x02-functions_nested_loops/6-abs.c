67e48cc4814c762fdb5555e8fefcee0bb8537062
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-05 07:33:43 -0700

    Prints absolute value of numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Prints the absolute value of numbers
+ *
+ * Return: 0 in success
+ *
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (a * -1);
+	}
+	else
+	{
+		return (a);
+	}
+}
