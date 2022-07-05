72c3f8ee30eb6ad6de310b8325036b2c30cd5135
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-07-05 20:22:40 +0300

    Updates

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 
 /**
- * main - Printout fibonaccie numbers
+ * main - prints fibonaccie numbers
  *
  * Return: 0 in success
  *
  */
 int main(void)
 {
-	int c, boolean, boolean2;
-	long int n1, n2, fn, fn2, n11, n22;
+	long int fs, fs2, num11, num22, num1, num2;
+	int i, b, c;
 
-	n1 = 1;
-	n2 = 2;
-	boolean =  boolean2 = 1;
-	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 96; c++)
+	num1 = 1;
+	num2 = 2;
+	b =  c = 1;
+	printf("%ld, %ld", num1, num2);
+	for (i = 0; i < 96; i++)
 	{
-		if (boolean)
+		if (b)
 		{
-			fn = n1 + n2;
-			printf(", %ld", fn);
-			n1 = n2;
-			n2 = fn;
+			fs = num1 + num2;
+			printf(", %ld", fs);
+			num1 = num2;
+			num2 = fs;
 		}
 		else
 		{
-			if (boolean2)
+			if (c)
 			{
-				n11 = n1 % 1000000000;
-				n22 = n2 % 1000000000;
-				n1 = n1 / 1000000000;
-				n2 = n2 / 1000000000;
-				boolean2 = 0;
+				num11 = num1 % 1000000000;
+				num22 = num2 % 1000000000;
+				num1 = num1 / 1000000000;
+				num2 = num2 / 1000000000;
+				c = 0;
 			}
-			fn2 = (n11 + n22);
-			fn = n1 + n2 + (fn2 / 1000000000);
-			printf(", %ld", fn);
-			printf("%ld", fn2 % 1000000000);
-			n1 = n2;
-			n11 = n22;
-			n2 = fn;
-			n22 = (fn2 % 1000000000);
+			fs2 = (num11 + num22);
+			fs = num1 + num2 + (fs2 / 1000000000);
+			printf(", %ld", fs);
+			printf("%ld", fs2 % 1000000000);
+			num1 = num2;
+			num11 = num22;
+			num2 = fs;
+			num22 = (fs2 % 1000000000);
 		}
-		if (((n1 + n2) < 0) && boolean == 1)
-			boolean = 0;
+		if (((num1 + num2) < 0) && b == 1)
+			b = 0;
 	}
 	printf("\n");
 	return (0);
