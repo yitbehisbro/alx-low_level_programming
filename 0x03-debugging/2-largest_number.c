006d1e73c44185b78a9e46df5a564add91a37f52
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-07-06 14:29:03 +0300

    Largest one

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,17 +16,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if ((b > a) && (a > c))
+	else if ((b > a) && (b > c))
 	{
 		largest = b;
 	}
-	else if ((c > b) && (b > a))
+	else
 	{
 		largest = c;
 	}
-	else
-	{
 
-	}
 	return (largest);
 }
