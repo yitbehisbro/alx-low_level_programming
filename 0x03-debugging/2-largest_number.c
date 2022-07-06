a90f019916ea2ab3b56d0848d4f184bf425d0d61
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-06 04:27:54 -0700

    Largest one

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b) && (b > c))
+	if ((a > b) && (a > c))
 	{
 		largest = a;
 	}
@@ -28,5 +28,5 @@ int largest_number(int a, int b, int c)
 	{
 
 	}
-	return (largest)
+	return (largest);
 }
