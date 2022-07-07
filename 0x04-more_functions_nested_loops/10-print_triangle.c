b4184d747d0006e388ab9b3a4ef014e0f71a8500
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-07 09:12:36 -0700

    Print triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints the triangle
+ *
+ * Return: 0 in success
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 1; b < (size - a); b++)
+			_putchar(' ');
+
+		for (b--; b < size; b++)
+			_putchar(35);
+		if (a < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
