9a43ae1409dcc1242bca42467b0190ed24906205
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-07 06:17:53 -0700

    Prints line in n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line n long
+ * @n: variable which hold n number of hyphen
+ * Return: no return for void
+ */
+void print_line(int n)
+{
+	int m;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (m = 0; m <= n; m++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
