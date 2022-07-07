8d6e36f57bf4a43de5192399072994c16d6fa9de
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-07 04:00:46 -0700

    Prints multiplication of two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integer
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 in success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
