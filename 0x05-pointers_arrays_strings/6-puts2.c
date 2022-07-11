fd188a7e54eeb92ca6297218187f79afefaac890
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-11 05:59:49 -0700

    Prints some characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints some charactes
+ * @str: holds string value
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int length;
+
+	length = 0;
+
+	while (length >= 0)
+	{
+		if (str[length] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (str[length] % 2 == 0)
+			_putchar(str[length]);
+		length++;
+	}
+}
