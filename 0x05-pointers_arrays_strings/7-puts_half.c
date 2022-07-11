e76f4b32b5d6b2701995cc66e7fa228e3a67ba76
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-11 06:27:46 -0700

    Prints half of the given strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half charactes
+ * @str: holds string value
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int length, size, length2;
+
+	length = 0;
+	
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+	size = length;
+
+	if (size % 2 == 0)
+	{
+		length2 = size / 2;
+
+		while (length2 >= 0)
+		{
+			if (str[length2] == '\0')
+			{
+				_putchar('\n');
+				break;
+			}
+			_putchar(str[length2]);
+			length2++;
+		}
+	}
+	else
+	{
+		length2 = (size - 1) / 2;
+
+		while (length2 >= 0)
+		{
+			if (str[length2] == '\0')
+			{
+				_putchar('\n');
+				break;
+			}
+			_putchar(str[length2]);
+			length2++;
+		}
+	}		
+}
