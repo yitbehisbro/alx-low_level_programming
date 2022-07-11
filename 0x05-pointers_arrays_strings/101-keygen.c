b1851faefe3d15681c76e60ca3b595c2ec409bfc
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-11 09:23:33 -0700

    Generate a valid password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords 
+ *
+ * Return: 0 Always.
+ */
+int main(void)
+{
+	time_t t;
+	int crack, random;
+
+	srand((unsigned int) time(&t));
+
+	while (crack < 2772)
+	{
+		random = rand() % 128;
+
+		if ((crack + random) > 2772)
+			break;
+
+		crack += random;
+		printf("%c", random);
+	}
+	printf("%c\n", (2772 - crack));
+	return (0);
+}
