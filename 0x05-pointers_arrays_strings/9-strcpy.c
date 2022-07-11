200a97fb7c4ed6609106f14b430d238e81a2d4f8
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-11 07:58:52 -0700

    Copy strings to the pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @desc: variable that holds the value of variable
+ * @src: sting values
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length, a, size;
+
+	length = 0;
+
+	while (*(src + length) != '\0')
+	{
+		length++;
+	}
+	size = length;
+
+	for (a = 0; a < size && src[a] != '\0'; a++)
+		dest[a] = src[a];
+
+	for ( ; a < size; a++)
+		dest[a] = '\0';
+	return (dest);
+}
