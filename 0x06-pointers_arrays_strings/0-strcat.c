17dc11895416b96db824265e66a5b8fff2211380
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-13 04:28:31 -0700

    Concatnates strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate strings
+ * @dest: pointer to character variable 1
+ * @src: pointer to character variable 2
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	while(*dest != '\0')
+	{
+		dest++;
+	}
+
+	while(*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (dest);
+}
