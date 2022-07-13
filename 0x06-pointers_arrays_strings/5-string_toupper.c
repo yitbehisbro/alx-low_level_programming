6f996cae8e9893cdc861b057e527be2773168613
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-13 07:05:33 -0700

    To UPPER Case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase to upper-case
+ * @s: pointer to char variable
+ * Return: Always 0.
+ */
+char *string_toupper(char *cs)
+{
+	int length;
+
+	length = 0;
+
+	while (*(cs + length) != '\0')
+	{
+		if ((*(cs + length) >= 97) && (*(cs + length) <= 122))
+			*(cs + length) -= 32;
+		length++;
+	}
+	return (cs);
+}
