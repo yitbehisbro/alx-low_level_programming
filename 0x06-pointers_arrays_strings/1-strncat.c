831fef790abc48f7fc8d1b56410f3d2fae0b7910
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-13 10:32:41 -0700

    Concatnate the string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strncat - concatenet two string by using at most n bytes from src
- * @src: pointer to character variable 1
- * @dest: pointer to character variable 2
+ * @src: pointer to character variable one
+ * @dest: pointer to character variable two
  * @n: integer values for bytes
  * Return: Always 0.
  */
@@ -17,10 +17,11 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		length++;
 	}
+
 	dest_size = length;
 
 	for (a = 0; a < n && *(src + a) != '\0'; a++)
-                   *(dest + (dest_size + a)) = *(src + a);
+		*(dest + (dest_size + a)) = *(src + a);
 	*(dest + (dest_size + a)) = '\0';
 
 	return (dest);
