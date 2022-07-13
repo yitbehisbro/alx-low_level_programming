4b23eb43d5ee3c448405a80806372a2f5fff897a
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-13 10:38:34 -0700

    Concatnet the string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,8 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	char *doit = dest;
+
 	while(*dest != '\0')
 	{
 		dest++;
@@ -21,5 +23,5 @@ char *_strcat(char *dest, char *src)
 	}
 	*dest = '\0';
 
-	return (dest);
+	return (doit);
 }
