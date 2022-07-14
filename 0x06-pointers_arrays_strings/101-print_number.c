fa0c04ff353e9da138aa0402f36cd9003db00153
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-14 03:37:36 -0700

    Print long integers with _putchar()

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - print integers
+ * @n: variable that holds integer values
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
