cc2aa8d682556a17e4ae90c37b85bb08f9246174
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-14 03:21:40 -0700

    Capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,14 +7,26 @@
  */
 char *cap_string(char *cs)
 {
-	int length;
+	int length, i;
+	int separtors[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	length = 0;
 
+	if (*(cs + length) >= 97 && *(cs + length) <= 122)
+		*(cs + length) = *(cs + length) - 32;
+	length++;
+
 	while (*(cs + length) != '\0')
 	{
-		if ((*(cs + length) >= 97 && *(cs + length) <= 122) && (*((cs + length) - 1) == 32 || *((cs + length) - 1) == 9 || *((cs + length) - 1) == 10 || *((cs + length) - 1) == 125 || *((cs + length) - 1) == 123 || *((cs + length) - 1) == 46 || *((cs + length) - 1) == 44 || *((cs + length) - 1) == 63 || *((cs + length) - 1) == 33 || *((cs + length) - 1) == 59 || *((cs + length) - 1) == 40 || *((cs + length) - 1) == 41 || *((cs + length) - 1) == 33))
-			*(cs + length) -= 32;
+		for (i = 0; i < 13; i++)
+		{
+			if (*(cs + length) == separtors[i])
+			{
+				if ((*(cs + (length + 1)) >= 97) && (*(cs + (length + 1)) <= 122))
+					*(cs + (length + 1)) -= 32;
+				break;
+			}
+		}
 		length++;
 	}
 	return (cs);
