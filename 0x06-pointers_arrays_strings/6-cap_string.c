198c78019b1eb6f942cd6dd8c377381aeaf9e3aa
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-14 02:50:28 -0700

    Capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *cs)
 
 	while (*(cs + length) != '\0')
 	{
-		if ((*(cs + length) >= 97) && (*(cs + length) <= 122) && (*((cs + length) - 1) == '\n' || *((cs + length) - 1) == '\t' || *((cs + length) - 1) == ' ' || *((cs + length) - 1) == ',' || *((cs + length) - 1) == '-' || *((cs + length) - 1) == '.' || *((cs + length) - 1) == '/' || *((cs + length) - 1) == '(' || *((cs + length) - 1) == ')' || *((cs + length) - 1) == '!' || *((cs + length) - 1) == '?' || *((cs + length) - 1) == '\"' || *((cs + length) - 1) == ';' || *((cs + length) - 1) == '}' || *((cs + length) - 1) == '{'))
+		if ((*(cs + length) >= 97) && (*(cs + length) <= 122) && (*((cs + length) - 1) == ' ' || *((cs + length) - 1) == '\t' || *((cs + length) - 1) == '\n' || *((cs + length) - 1) == '}' || *((cs + length) - 1) == '{' || *((cs + length) - 1) == '.' || *((cs + length) - 1) == ',' || *((cs + length) - 1) == '?' || *((cs + length) - 1) == '!' || *((cs + length) - 1) == ';' || *((cs + length) - 1) == '(' || *((cs + length) - 1) == ')' || *((cs + length) - 1) == '"'))
 			*(cs + length) -= 32;
 		length++;
 	}
