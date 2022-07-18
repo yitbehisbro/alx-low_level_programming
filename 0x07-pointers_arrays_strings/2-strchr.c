e1945811ff6afb35cf48e40bb773e90f4b6b460b
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-18 05:04:57 -0700

    Search for the first occernace of a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - find the first occernance of c
+ * @s: ponter to char (string)
+ * @c: character to be find
+ * Return: pointer to s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	do
+	{
+		if (*s == c)
+			return (s);
+	}
+	while (*s++);
+
+	return ('\0');
+}
