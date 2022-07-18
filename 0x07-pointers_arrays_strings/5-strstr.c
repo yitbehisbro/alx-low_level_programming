47644af3d98c86342c36aac03e1428d31fab7f6e
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-18 07:28:31 -0700

    Prints a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of the
+ * substring needle in the string haystack
+ *
+ * @haystack: string to be scanned
+ * @needle: substring to scan
+ *
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*haystack && *b && *haystack == *b)
+		{
+			haystack++;
+			b++;
+		}
+		if (!*b)
+			return (a);
+		haystack = a + 1;
+	  }
+	  return ('\0');
+}
