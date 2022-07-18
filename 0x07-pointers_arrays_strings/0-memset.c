7835df316b5916ea7dbd831c2b4d503852a91aa7
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-18 04:27:16 -0700

    Fill memmory with block of values

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,17 @@
+#include "main.h"
 /**
  * _memset - fills memory with a constant byte
- *
- * @s: pointer to the block of memory to fill
- *
+ * @s: pointer to the block of memory to be filled
  * @b: fills the block of memory using the unsigned char conversion of this value
- *
  * @n: number of bytes to be set to the value
- *
- * Return: 0 in success
- *
+ * Return: the pointer to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
-		*(s + a) = b;
+		*(s + a) =  b;
 
 	return (s);
 }
