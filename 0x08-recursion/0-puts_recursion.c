835ca1f9d14a0e2453cac9577e1d3d1e1cd80a40
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-19 02:36:32 -0700

    Print strings

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: char variable that holds string value to be print on stdout
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	int length;
+
+	length = 0;
+
+	if (*(s + length) == '\0')
+	{
+		_putchar('\n');
+		return ;
+	}
+	_putchar(*(s + length));
+	length++;
+	_puts_recursion(s + length);
+}
