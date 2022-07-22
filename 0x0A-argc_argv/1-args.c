48499bfd4668ee223e171aa1f9d630a928f043d9
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-22 04:49:25 -0700

    Prints number of passed arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of program
+ * main - prints the number of arguments
  * @argc: argument that holds the count
  * @argv: vector that holds string
  * Return: 0 in success
  */
 int main (int argc, __attribute__((unused)) char *argv[])
 {
-	if (argc > 0)
+	if (argc >= 0)
 	{
 		printf("%d\n", argc - 1);
 	}
 	else
 	{
-		return (0);
+		return (-1);
 	}
 	return (0);
 }
