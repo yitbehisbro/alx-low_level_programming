e2c3470986d83815d858954243495c06fa467e99
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-22 04:57:13 -0700

    Prints the name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,14 @@
  * main - prints the name of program
  * @argc: argument that holds the count
  * @argv: vector that holds string
- * Return: 0 in success
+ * Return: 0 - in success
  */
-int main (int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	if (argc < 0)
+	{
+		return (-1);
+	}
 	printf("%s\n", argv[0]);
 	return (0);
 }
