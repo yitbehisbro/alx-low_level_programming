d7ab1fd3a6eef5fd686f6d480ddd9d0ec794b027
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-28 07:43:07 -0700

    Coustomized realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,18 +12,20 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == old_size)
-		return (ptr);
-
-	if (ptr == NULL)
-		ptr = malloc(new_size);
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	ptr = malloc(new_size);
-	return (ptr);
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
+
 	free(ptr);
+	ptr = malloc(new_size);
+
+	return (ptr);
 }
