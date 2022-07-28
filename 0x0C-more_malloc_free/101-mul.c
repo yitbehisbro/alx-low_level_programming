35914fddaf423e5b7a8a2ecefbc61e14ab183cc3
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-07-28 09:16:26 -0700

    Multply two positive integers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,29 +4,29 @@
 #include <ctype.h>
 
 /**
- * if_zero - determines if a number is 0
+ * _if_zero - determines if a number is 0
  * @argv: vector
  * Return: no return
  */
-void if_zero(char *argv[])
+void _if_zero(char *argv[])
 {
-	int a, n1 = 1, n2 = 1;
+	int i, isn1 = 1, isn2 = 1;
 
-	for (a = 0; argv[1][a]; a++)
-		if (argv[1][a] != '0')
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
 		{
-			n1 = 0;
+			isn1 = 0;
 			break;
 		}
 
-	for (a = 0; argv[2][a]; a++)
-		if (argv[2][a] != '0')
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
 		{
-			n2 = 0;
+			isn2 = 0;
 			break;
 		}
 
-	if (n1 == 1 || n2 == 1)
+	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
 		exit(0);
@@ -39,34 +39,34 @@ void if_zero(char *argv[])
  * @size: size of array
  * Return: pointer to array
  */
-char *_initialize(char *ary, int size)
+char *_initialize(char *ar, int lar)
 {
-	int c = 0;
+	int i = 0;
 
-	for (c = 0; c < size; c++)
-		ary[c] = '0';
-	ary[size] = '\0';
-	return (ary);
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
 }
 
 /**
- * checkpoint - determine length and sign
+ * _checkpoint - determine length and sign
  * @argv: vector arg
  * @r: num of row
  * Return: size of number
  */
-int checkpoint(char *argv[], int r)
+int _checkpoint(char *argv[], int n)
 {
-	int size;
+	int ln;
 
-	for (size = 0; argv[r][size]; size++)
-		if (!isdigit(argv[r][size]))
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
 
-	return (size);
+	return (ln);
 }
 
 /**
@@ -77,44 +77,44 @@ int checkpoint(char *argv[], int r)
  */
 int main(int argc, char *argv[])
 {
-	char *mymult;
-	int n1, n2, out, a, ca, al, x, y, z;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	n1 = checkpoint(argv, 1), n2 = checkpoint(argv, 2);
-	if_zero(argv), out = n1 + n2, mymult = malloc(out + 1);
-	if (mymult == NULL)
+	ln1 = _checkpoint(argv, 1), ln2 = _checkpoint(argv, 2);
+	_if_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
 		printf("Error\n"), exit(98);
-	mymult = _initialize(mymult, out);
-	z = out - 1, x = n1 - 1, y = n2 - 1, ca = al = 0;
-	for (; z >= 0; z--, x--)
+	nout = _initialize(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		if (x < 0)
+		if (i < 0)
 		{
-			if (al > 0)
+			if (addl > 0)
 			{
-				a = (mymult[z] - '0') + al;
-				if (a > 9)
-					mymult[z - 1] = (a / 10) + '0';
-				mymult[z] = (a % 10) + '0';
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
 			}
-			x = n1 - 1, y--, al = 0, ca++, z = out - (1 + ca);
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
-		if (y < 0)
+		if (j < 0)
 		{
-			if (mymult[0] != '0')
+			if (nout[0] != '0')
 				break;
-			out--;
-			free(mymult), mymult = malloc(out + 1), mymult = _initialize(mymult, out);
-			z = out - 1, x = n1 - 1, y = n2 - 1, ca = al = 0;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
-		if (y >= 0)
+		if (j >= 0)
 		{
-			a = ((argv[1][x] - '0') * (argv[2][y] - '0')) + (mymult[z] - '0') + al;
-			al = a / 10, mymult[z] = (a % 10) + '0';
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
-	printf("%s\n", mymult);
+	printf("%s\n", nout);
 	return (0);
 }
