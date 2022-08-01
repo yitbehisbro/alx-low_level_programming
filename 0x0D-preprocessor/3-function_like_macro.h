4b3ba3119707e99e63e200f0d67b13ebef4a77f1
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-01 06:15:10 -0700

    Prints the Absoulute value of numbers

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,6 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H
 #define _3_FUNCTION_LIKE_MACRO_H
 
-#define N (-1)
-#define Y 0
-#define ABS(x) ((x) == (Y) ? (x) : ((x) < (Y) ? (N * x) : (x)))
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
