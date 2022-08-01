41a359e4898fc998a18824cee1942ecc45a1b4fa
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-01 07:14:34 -0700

    Prints the profile of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - holds dog profile
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: No need
+ */
+typedef struct dog
+{
+	char	*name;
+	float	age;
+	char	*owner;
+} my_dog;
+
+#endif
