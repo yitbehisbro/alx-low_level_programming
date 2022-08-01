7122901225120c2f9b7f2ee3a2f12bbd5e8565b9
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-01 08:03:45 -0700

    Print the dog details

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,22 +8,18 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	{
-		return;
-	}
-
 	if (d)
 	{
-		if (d->name == NULL)
-		{
-			printf("%s: (nil)\n", d->name);
-		}
-	
-		if (d->owner == NULL)
-		{
-			printf("%s: (nil)\n", d->owner);
-		}
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		if (!(d->name))
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
