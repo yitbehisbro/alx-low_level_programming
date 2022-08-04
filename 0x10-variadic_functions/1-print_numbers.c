8c37916dc9653c2fc95bbcecb17efbdc924c8b83
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-04 08:00:33 -0700

    Prints any given numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int p;
 	va_list vlist;
+
 	va_start(vlist, n);
 
-	if (separator != NULL)
+	for (p = 0; p < n; p++)
 	{
-		for (p = 0; p < n; p++)
-		{
-			printf("%d", va_arg(vlist, int));
-			if (p == (n - 1))
-				continue;
-			printf("%s", separator);
-		}
+		printf("%d", va_arg(vlist, int));
+
+		if (p == (n - 1) || separator == NULL)
+			continue;
+
+		printf("%s", separator);
 	}
 	va_end(vlist);
 	printf("\n");
