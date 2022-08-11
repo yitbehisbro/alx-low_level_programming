f3ae9b1adfd278e0f2734808b8350b00de6413f7
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-11 05:23:20 -0700

    Add nodes to the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,10 +31,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = new_node;
+	}
 	else
 	{
 		list_t *last_node = *head;
+
 		while (last_node->next != NULL)
 		{
 			last_node = last_node->next;
