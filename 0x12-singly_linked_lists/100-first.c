0d22c48811325d0bba08fbef28b7626955c9cc89
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-11 06:51:51 -0700

    Prints text before main function do

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
-#include "lists.h"
 #include <stdio.h>
 
 /**
- * before_main - prints text before main
+ * very_before - prints text before main
+ *
  * Return: no return
  */
-void __attribute__ ((destructor)) before_main()
+
+void __attribute__ ((constructor)) very_before()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
