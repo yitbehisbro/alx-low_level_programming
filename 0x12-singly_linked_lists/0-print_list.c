0cd66c68aa4d6561b3961a066aa89010ac7ff76e
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-11 04:17:24 -0700

    Prints the all list of the linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,12 +15,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
-		cntr++;
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		cntr++;
 	}
 	return (cntr);
 }
