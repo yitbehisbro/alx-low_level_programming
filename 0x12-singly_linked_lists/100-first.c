95b778b8bdfb1e4b9ba1c827103062d02206aad3
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-11 06:03:19 -0700

    Print strings before main function do

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+/**
+ * before_main - prints text before main
+ *
+ * Return: no return
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
