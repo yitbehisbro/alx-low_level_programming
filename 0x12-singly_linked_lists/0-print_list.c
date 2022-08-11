0984d2fa7c1211dcb098be998f126752dc44a2f3
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-11 02:36:39 -0700

    Prints the list of linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints the list of lists
+ * @h: pointer to the struct
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t cntr = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		cntr++;
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (cntr);
+}
