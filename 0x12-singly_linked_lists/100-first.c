b41ce6c3af326faa06e1f8b987da3cdf823e886f
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-11 06:15:25 -0700

    Print strings before main function do

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,8 @@
  * before_main - prints text before main
  * Return: no return
  */
-void before_main(void) __attribute__ ((destructor))
+void before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
