b02f7abf90419a0073b62d9f9da136ef0fa70210
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-11 04:28:57 -0700

    Prints the length of the elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,9 +4,8 @@
 #include "lists.h"
 
 /**
- * list_len - prints length of the elements
+ * list_len - prints size of elements
  * @h: pointer to the struct
- *
  * Return: Always 0.
  */
 size_t list_len(const list_t *h)
@@ -18,5 +17,5 @@ size_t list_len(const list_t *h)
 		cntr++;
 		h = h->next;
 	}
-	return(cntr);
+	return (cntr);
 }
