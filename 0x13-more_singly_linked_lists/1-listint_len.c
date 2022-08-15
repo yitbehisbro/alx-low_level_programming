70f534aca6e0e8dff81b4698de0c67f71b07e9b2
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-15 02:07:56 -0700

    Prints the size of Linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - prints number of the elements 
+ * @h: pointer to the struct
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+        size_t cntr = 0;
+
+	while (h != NULL)
+	{
+		cntr++;
+                h = h->next;
+	}
+	return (cntr);
+}
