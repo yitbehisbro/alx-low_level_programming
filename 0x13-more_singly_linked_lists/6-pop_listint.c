139f6723933f656942b166a542d2a7b0393200c2
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-15 04:28:47 -0700

    Deletes the nodes from the Linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - pops the head node of a linked list
+ * @head: pointer to the struct
+ *
+ * Return: the head node’s data (n)
+ */
+int pop_listint(listint_t **head)
+{
+        listint_t *h = *head;
+        int backup;
+
+        if (*head == NULL)
+        {
+                return (0);
+        }
+        backup = h->n;
+        (*head) = (*head)->next;
+
+        free(h);
+
+        return (backup);
+}
