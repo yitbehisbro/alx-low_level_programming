9f033e09920df146acd45423800c955f4fe25768
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-16 12:49:11 +0300

    Prints all the nodesof the linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,9 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t cntr = 0, cmpr = 0;
+	size_t cntr = 0;
 
 	while (h != NULL)
-	{
-		h = h->next;
-		cmpr++;
-	}
-
-	while (h != NULL && cntr <= cmpr)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
