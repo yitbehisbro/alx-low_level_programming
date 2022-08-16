ca0a60c51f65375291b3a5fc3c35732a5de76e17
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-16 12:12:07 +0300

    Prints total number of nodes

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,14 +2,15 @@
 #include "lists.h"
 
 /**
- * listint_len - prints number of the elements
+ * listint_len - prints number of the
+ * elements
  * @h: pointer to the struct
  *
  * Return: number of elements
  */
 size_t listint_len(const listint_t *h)
 {
-        size_t cntr = 0;
+	size_t cntr = 0;
 
 	while (h != NULL)
 	{
