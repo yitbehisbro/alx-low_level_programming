ec8c4afd9684d1778b09bdb469ff7c463bba4f38
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-16 13:48:06 +0300

    Prints the nth node of a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,13 +9,16 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int cntr = 0;
+	unsigned int a;
 
-	while (cntr <= index && head != NULL)
+	if (head == NULL)
+		return (NULL);
+
+	for (a = 0; a < index; a++)
 	{
-                cntr++;
 		head = head->next;
+		if (head == NULL)
+			return (NULL);
 	}
-
 	return (head);
 }
