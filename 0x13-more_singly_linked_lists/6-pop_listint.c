e28257065baf753f41e25a2d24af37d52da9cc11
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-16 13:50:06 +0300

    Pops the head node of a linked list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -16,7 +16,7 @@ int pop_listint(listint_t **head)
 
         if (*head == NULL)
         {
-                return (0);
+                return (NULL);
         }
         backup = h->n;
         (*head) = (*head)->next;
