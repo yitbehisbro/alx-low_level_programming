c478fcd646eaef2d72340aaedee5ce372e574245
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-17 07:16:11 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t cntr = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		cntr++;
 	}
-	return (count);
+	return (cntr);
 }
