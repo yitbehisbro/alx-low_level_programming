bab20fff9d974900f62fccece98354fe78adbbd6
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-18 12:35:51 +0300

    Updates

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
 /**
  * binary_to_uint -  converts a binary number to an unsigned int.
  * @b: pointing to a string of 0 and 1 chars.
- * 
+ *
  * Return: the converted number, or 0 if there is
  * one or more chars in the string b that is not 0 or 1
  * b is NULL
