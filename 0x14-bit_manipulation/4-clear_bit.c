4aedb8e58e82d2a1aec21abe3c2ce0c173850b8a
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-18 15:43:09 +0300

    Sets the value of a bit to 0 at a given index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,18 +2,18 @@
 
 /**
  * clear_bit - sets the value of a bit to 0 at a given index.
- * @n: number to set
+ * @n: number pointer to set
  * @index: index at which to set bit
  *
  * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int set;
+	unsigned long int order;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	set = ~(1 << index);
-	*n = *n & set;
+	order = ~(1 << index);
+	*n = *n & order;
 	return (1);
 }
