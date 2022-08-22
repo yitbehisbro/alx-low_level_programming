e866f37207b4e4ec21db445221ba0257d9e52928
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-22 05:21:06 -0700

    Copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,40 +19,49 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-	exit(97);
-
+		exit(97);
+	}
 	cp_this = open(argv[1], O_RDONLY);
 	if (cp_this == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
-
+		exit(98);
+	}
 	to_this = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (to_this == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	exit(99);
-
+		exit(99);
+	}
 	count_ch = 1024;
 	while (count_ch == 1024)
 	{
 		count_ch = read(cp_this, buffer, 1024);
 		if (count_ch == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-
+			exit(98);
+		}
 		count_fd = write(to_this, buffer, count_ch);
 		if (count_fd == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+			exit(99);
+		}
 	}
 	close_in_error = close(cp_this);
 	if (close_in_error == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_this);
-	exit(100);
-
+		exit(100);
+	}
 	close_in_error = close(to_this);
 	if (close_in_error == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_this);
-	exit(100);
+		exit(100);
+	}
 	return (0);
 }
