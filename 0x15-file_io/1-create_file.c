fe2a12d1c8fcc112b3b87b9eeab4ee9de16f0f59
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-08-22 03:28:45 -0700

    Creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,18 +22,30 @@ int create_file(const char *filename, char *text_content)
 
 	/** To get the size of text_content */
 	while (*(text_content + size) != '\0')
+	{
 		size++;
+	}
 	if (filename == NULL)
+	{
 		return (-1);
+	}
 	if (text_content == NULL)
+	{
 		return (-1);
-	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	}
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_d == -1)
+	{
 		return (-1);
+	}
 	if (text_content != NULL)
+	{
 		length = write(file_d, text_content, size);
+	}
 	close(file_d);
 	if (length == -1)
+	{
 		return (-1);
+	}
 	return (1);
 }
