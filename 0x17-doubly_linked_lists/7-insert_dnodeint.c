aa0a147b2f22da8768b2d28294856c6d57fef003
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-09-29 04:40:45 -0700

    Inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,39 +10,41 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *new;
+	dlistint_t *new_node = NULL;
 	dlistint_t *head;
+	unsigned int a;
 
-	head = *h;
-
-	if (idx != 0)
+	if (idx == 0)
+		new_node = add_dnodeint(h, n);
+	else
 	{
-		for (i = 0; i < idx - 1 && head != NULL; i++)
+		head = *h;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		for (a = 1; head != NULL; a++)
 		{
+			if (a == idx)
+			{
+				if (head->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->next = head->next;
+						new_node->prev = head;
+						head->next->prev = new_node;
+						head->next = new_node;
+					}
+				}
+				break;
+			}
 			head = head->next;
 		}
 	}
 
-	if (head == NULL && idx != 0)
-		return (NULL);
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-
-	if (idx == 0)
-	{
-		new->next = *h;
-		*h = new;
-	}
-	else
-	{
-		new->next = head->next;
-		head->next = new;
-	}
-
-	return (new);
+	return (new_node);
 }
