ce401b1e280162530ef3e704d729ad692e1d9b6f
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-03 08:28:45 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/yitbehisbro/alx-low_level_programming/master/0x18-dynamic_libraries/mygames.so
-export LD_PRELOAD=/tmp/mygames.so
+wget -P /tmp/ https://raw.github.com/yitbehisbro/alx-low_level_programming/master/0x18-dynamic_libraries/myshared.so
+export LD_PRELOAD=/tmp/myshared.so
