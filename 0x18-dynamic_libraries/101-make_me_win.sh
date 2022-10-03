8a30644bb3ee1a8f703c5cba8d4486b8493e9be2
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-03 17:57:00 +0300

    Updates

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/yitbehisbro/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lotto.so?raw=true
-export LD_PRELOAD=/tmp/lotto.so
+wget -P /tmp/ https://github.com/yitbehisbro/alx-low_level_programming/blob/master/0x18-dynamic_libraries/games.so?raw=true
+export LD_PRELOAD=/tmp/games.so
