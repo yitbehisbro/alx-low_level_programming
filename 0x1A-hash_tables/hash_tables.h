5ffaab92d73689a83cc5eeb56a795c705b80185b
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-21 02:25:17 -0700

    0x1A. C - Hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -54,11 +54,11 @@ void hash_table_delete(hash_table_t *ht);
  */
 typedef struct shash_node_s
 {
-     char *key;
-     char *value;
-     struct shash_node_s *next;
-     struct shash_node_s *sprev;
-     struct shash_node_s *snext;
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
 } shash_node_t;
 
 /**
@@ -73,10 +73,10 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-     unsigned long int size;
-     shash_node_t **array;
-     shash_node_t *shead;
-     shash_node_t *stail;
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
 } shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
@@ -85,7 +85,7 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
-shash_node_t *add_n_shash(shash_node_t **h, const char *key, const char *value);
+shash_node_t *add_n_shash(shash_node_t **h, const char *k, const char *v);
 void add_i_shash(shash_table_t *ht, shash_node_t *new);
 
 #endif
