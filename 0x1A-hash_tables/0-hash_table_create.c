1356b6e251bee97ceabf01822a3e6e83090abd5f
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-21 11:16:30 +0300

    Creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -26,3 +26,30 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	return (table);
 }
+
+/**
+ * shash_table_create - creates a hash table.
+ * @size: the size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ * else returns NULL
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_node_t **array = malloc(sizeof(hash_node_t *) * size);
+	unsigned long int i = 0;
+
+	if (table == NULL || array == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		array[i] = NULL;
+		i++;
+	}
+	table->array = array;
+	table->size = size;
+
+	return (table);
+}
