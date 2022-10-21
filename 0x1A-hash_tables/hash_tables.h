05a2954656f914aa87fba1b5d25e2831a410b6f8
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-21 11:34:56 +0300

    Content added

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -61,5 +61,7 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+shash_node_t *add_n_shash(shash_node_t **h, const char *key, const char *value);
+void add_i_shash(shash_table_t *ht, shash_node_t *new);
 
 #endif
