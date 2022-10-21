2cf70aab8246c1482a6d5f96623b422b1c17e3de
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2022-10-21 02:21:19 -0700

    0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_create - creates a hash table.
- * @size: the size of the array
+ * @size: size of the array
  *
  * Return: a pointer to the newly created hash table
  * else returns NULL
