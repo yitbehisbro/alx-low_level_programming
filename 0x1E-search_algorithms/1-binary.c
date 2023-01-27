7a94756a4edbfd2a30f8dcc1409c5eb0d31765fc
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-27 10:37:41 +0300

    Error fixed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,27 +1,41 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * display - display the searched numbers
- * @size: the size
- * @min: lowest value
- * @array: the array of numbers
+ * recursive_search -  searches for a value in an array of integers
+ * using the recursive search algorithm
+ * @array: a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: the value to search for
  *
+ * Return: index of the number
  */
-void display(size_t size, int *array, size_t min)
+int recursive_search(int *array, size_t size, int value)
 {
-	size_t i, k;
+	size_t half = size / 2;
+	size_t i;
 
-	for (i = min; i <= size; i++)
-	{
-		if (i == size)
-		{
-			printf("Searching in array");
-			for (k = min; k <= size; k++)
-				printf("%s %d", (k == min) ? ":" : ",", array[k]);
-			printf("\n");
-		}
-	}
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
 }
 
 /**
@@ -35,34 +49,12 @@ void display(size_t size, int *array, size_t min)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left, right, mid;
+	int index;
 
-	if (array != NULL)
-	{
-		left = array[0];
-		right = array[size - 1];
+	index = recursive_search(array, size, value);
 
-		display(size - 1, array, 0);
+	if (index >= 0 && array[index] != value)
+		return (-1);
 
-		while (left <= right)
-		{
-			mid = floor((left + right) / 2);
-
-			if (array[mid] < value)
-			{
-				left = mid + 1;
-				display(right, array, left);
-			}
-			else if (array[mid] > value)
-			{
-				right = mid - 1;
-				display(mid - 1, array, 0);
-			}
-			else
-			{
-				return (mid);
-			}
-		}
-	}
-	return (-1);
+	return (index);
 }
